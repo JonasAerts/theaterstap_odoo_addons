355a8ed66b0027dd065b91700a73eb5631e95384
jonas aerts <jonas.aerts@hotmail.com>
2025-02-21 11:22:01 +0100

    changes

--- xx_partner/__manifest__.py
+++ xx_partner/__manifest__.py
@@ -1,12 +1,15 @@
 {
     'name': "Stap Custom module",
+    'licence': 'GPL-3',
     'version': '18.0.1.0',
-    'depends': ['base'],
+    'depends': ['contacts'],
     'author': "Jonas Aerts",
     'category': 'Partner',
     # data files always loaded at installation
     'data': [
         "views/res_partner_views.xml",
     ],
-    'installable': True
+    "installable": True,
+    "application": True,
+    "auto_install": False,
 }
